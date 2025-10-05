e6da2a5f9082bffe0ecbea599e543b366b057541
Sri Balaji V <sribalajihema2003@gmail.com>
2025-10-05 19:07:56 +0530

    added expense service

---
+++ src/main/java/com/expensetracker/service/ExpenseService.java
@@ -0,0 +1,36 @@
+package com.expensetracker.service;
+
+import com.expensetracker.model.Expense;
+import java.util.*;
+
+public class ExpenseService {
+
+    private List<Expense> expenses;
+
+    public ExpenseService(){
+        this.expenses = new ArrayList<>();
+    }
+
+    //add a new expense
+    public void addExpense(Expense expense){
+        this.expenses = expenses;
+        System.out.println("Expense added succesfully");
+    }
+
+    //get all expenses
+    public List<Expense> getAllExpenses(){
+        return expenses;
+    }
+
+    //Display all expenses
+    public void displayAllExpenses(){
+        if(expenses.isEmpty()){
+            System.out.println("No expenses recorded yet");
+        } else {
+            System.out.println("\n All Expenses:");
+            for (Expense e: expenses){
+                System.out.println(e);
+            }
+        }
+    }
+}
